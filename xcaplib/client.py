0fee5ec8319608de8eb7cc0d0530d6908753f6c0
Denis Bilenko <denis@ag-projects.com>
2008-09-30 04:17:22 +0000

    spell check

--- xcaplib/client.py
+++ xcaplib/client.py
@@ -154,7 +154,7 @@ class HTTPConnectionWrapper(object):
             response.etag = parse_etag_value(response.headers.get('etag'))
             return response
             # contrary to what documentation for urllib2 says, this can return addinfourl
-            # instead of HTTPError which is though has all the relevant attributes (code, msg etc)
+            # instead of HTTPError which has all the relevant attributes (code, msg etc)
         except HTTPError, e:
             e.req = req
             e.etag = parse_etag_value(e.headers.get('etag'))
