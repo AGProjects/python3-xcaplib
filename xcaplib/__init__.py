bea7ced988a6d531bf22b2c4fec2fb6b559469f9
Denis Bilenko <denis@ag-projects.com>
2009-04-13 15:27:59 +0000

    set version to 1.0.9

--- xcaplib/__init__.py
+++ xcaplib/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2008-2009 AG Projects. See LICENSE for details.
 #
 
-__version__ = '1.0.8'
-version_info = (1, 0, 8, None, 0)
+__version__ = '1.0.9'
+version_info = (1, 0, 9, None, 0)
