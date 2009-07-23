0b7e4fd9662017c59d194e74f73b8810e8c8edae
Adrian Georgescu <ag@ag-projects.com>
2009-07-23 16:33:02 +0000

    Increased version

--- xcaplib/__init__.py
+++ xcaplib/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2008-2009 AG Projects. See LICENSE for details.
 #
 
-__version__ = '1.0.9'
-version_info = (1, 0, 9, None, 0)
+__version__ = '1.1.0'
+version_info = (1, 1, 0, None, 0)
