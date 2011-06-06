04bb52f4c8d6c87856f9a48dcc0061172d8d9d2f
Saul Ibarra <saul@ag-projects.com>
2011-06-06 12:59:15 +0000

    Raised version and updated changelog

--- xcaplib/__init__.py
+++ xcaplib/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2008-2010 AG Projects. See LICENSE for details.
 #
 
-__version__ = '1.0.15'
-version_info = (1, 0, 15, None, 0)
+__version__ = '1.0.16'
+version_info = (1, 0, 16, None, 0)
