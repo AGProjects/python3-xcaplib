a85fd811f4163a90c628761bf2aec6e472c4afb0
Dan Pascu <dan@ag-projects.com>
2012-07-12 19:48:58 +0000

    Increased version number and updated changelog

--- xcaplib/__init__.py
+++ xcaplib/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2008-2010 AG Projects. See LICENSE for details.
 #
 
-__version__ = '1.0.16'
-version_info = (1, 0, 16, None, 0)
+__version__ = '1.0.17'
+version_info = (1, 0, 17, None, 0)
