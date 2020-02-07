d5ea309f1f1158a7b3b2458bff7630ac2fc58133
Dan Pascu <dan@ag-projects.com>
2020-02-07 17:01:49 +0000

    Refactored setup.py for PEP-8 compliance

--- setup.py
+++ setup.py
@@ -3,20 +3,26 @@
 from distutils.core import setup
 from xcaplib import __version__
 
-setup(name         = "python-xcaplib",
-      version      = __version__,
-      author       = "AG Projects",
-      author_email = "support@ag-projects.com",
-      url          = "http://openxcap.org/",
-      description  = "Client for managing full or partial XML documents on XCAP servers (RFC 4825)",
-      license      = "LGPL",
-      platforms    = ["Platform Independent"],
-      classifiers  = [
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Service Providers",
-        "License :: OSI Approved :: Lesser General Public License (LGPL)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-      ],
-      packages = ['xcaplib'],
-      scripts  = ['xcapclient'])
+setup(
+    name='python-xcaplib',
+    version=__version__,
+
+    description='Client for managing full or partial XML documents on XCAP servers (RFC 4825)',
+    license='LGPL',
+    url='http://openxcap.org/',
+
+    author='AG Projects',
+    author_email='support@ag-projects.com',
+
+    platforms=['Platform Independent'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Service Providers',
+        'License :: OSI Approved :: Lesser General Public License (LGPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+    ],
+
+    packages=['xcaplib'],
+    scripts=['xcapclient']
+)
