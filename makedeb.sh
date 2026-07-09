73cc7f405f2a10fd113b5cbbc80e4b3fc5537236
Adrian Georgescu <ag@ag-projects.com>
2026-07-09 10:40:19 +0000

    Fix for makedeb descript

--- makedeb.sh
+++ makedeb.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
-if [ -f dist ]; then
-    rm -r dist
+set -e
+
+distro="${1:-}"
+mode="${2:-}"
+
+# rebuild mode: reuse the extracted build tree from a previous run;
+# skips clean (-nc) and builds binary packages only (-b)
+if [ "$mode" = "rebuild" ]; then
+    cd dist/*/
+    if [ -n "$distro" ] && [ "$distro" != "sid" ]; then
+        sed -i "s/) unstable/$distro) $distro/" debian/changelog
+        head -1 debian/changelog
+    fi
+    debuild --no-sign -nc -b
+    exit 0
 fi
 
+rm -rf dist build
 
-python3 setup.py sdist
-
+if [ -f setup.py ]; then
+    python3 setup.py sdist
+else
+    python3 -m build --sdist
+fi
 cd dist
 
-tar zxvf *.tar.gz
+tar zxf *.tar.gz
+cd */
 
-cd python3-xcaplib-?.?.?
+# add distro suffix to the changelog version (like autopackager does),
+# only in the extracted build tree - never in the source repo
+if [ -n "$distro" ] && [ "$distro" != "sid" ]; then
+    sed -i "s/) unstable/$distro) $distro/" debian/changelog
+    head -1 debian/changelog
+fi
 
 debuild --no-sign
-
-ls
